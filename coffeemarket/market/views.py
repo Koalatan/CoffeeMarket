bd0d2364e905f230348c24cd5a7bbbf796927cb2
AkaneHoashi <akane.hoashi@outlook.jp>
2019-10-28 23:47:47 +0900

    会員登録処理のお試し

--- coffeemarket/market/views.py
+++ coffeemarket/market/views.py
@@ -1,16 +1,30 @@
+from django.contrib import messages
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView
-from django.model import CreateRegister
+from django.http import HttpResponse, request
+from django.template import context
+from django.views import generic
+from .models import InsertRegister
+from .forms import Registration
 
 # Create your views here.
 
 
 class Register(generic.CreateView):
     # モデル指定
-    model = CreateRegister
+    model = InsertRegister
     # form.pyのクラスを指定
     form_class = Registration
     template_name = "create_register.html"
-    success_url = reverse_lazy('complete')
+
+    def form_valid(self, form):
+        return render(request, 'complete.html', {
+            'message': '認証に成功しました!'
+        })
+
+    def form_invalid(self, form):
+        return render(request, 'bad.html', {
+            'message': '認証に失敗しました！ \n 残念！'
+        })
+
+
 
