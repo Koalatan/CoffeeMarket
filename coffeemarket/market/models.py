1069e4891fe42da67ff95a9bfaaaeac1427f0686
AkaneHoashi <akane.hoashi@outlook.jp>
2019-12-06 21:28:04 +0900

    作成：コーヒー豆テーブル、admin.pyに表示できるよう設定

--- coffeemarket/market/models.py
+++ coffeemarket/market/models.py
@@ -9,3 +9,14 @@ class InsertRegister(models.Model):
     name = models.CharField(max_length=50)
     password = models.CharField(max_length=25)
 
+
+# コーヒー豆情報
+class CoffeeBeans(models.Model):
+    # id
+    beansCode = models.CharField(max_length=5, primary_key=True)
+    beansName = models.CharField(max_length=25)
+    place = models.CharField(max_length=25)
+    price = models.IntegerField(null=True, blank=True)
+    # 登録日付 **更新日付はauto_now
+    createDate = models.DateTimeField(auto_now_add=True)
+
