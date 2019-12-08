c768dacafb583eee3fd5595e6b21490bcc8528d5
AkaneHoashi <akane.hoashi@outlook.jp>
2019-12-09 01:16:14 +0900

    DBの作成、E-R図修正（Djangoは複合主キー非対応）

--- coffeemarket/market/admin.py
+++ coffeemarket/market/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import InsertRegister, CoffeeBeans
+from .models import InsertRegister, CoffeeBeans, PaymentMethod,\
+                    PurchaseDetail, PurchaseHistory
 # Register your models here.
 
 admin.site.register(InsertRegister)
 admin.site.register(CoffeeBeans)
+admin.site.register(PaymentMethod)
+admin.site.register(PurchaseDetail)
+admin.site.register(PurchaseHistory)
