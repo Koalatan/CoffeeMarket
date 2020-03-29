785c8217d31fe357e0c2f07fa892ef5e411b0147
hoashi <akane.hoashi@outlook.jp>
2020-03-30 00:47:00 +0900

    環境変更

--- coffeemarket/market/views.py
+++ coffeemarket/market/views.py
@@ -5,28 +5,10 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import InsertRegister, CoffeeBeans
-from .forms import Registration, LoginForm, InsertBeans
+from .forms import Registration, InsertBeans
 
 
 # Create your views here.
-
-
-class Register(generic.CreateView):
-    # 一覧表示
-    # モデル指定
-    model = InsertRegister
-    # form.pyのクラスを指定
-    form_class = Registration
-    template_name = "create_register.html"
-
-    # 重複していない場合
-    def form_valid(self, form):
-        form.save()
-        return redirect('market:insertResult', ans="good")
-
-    # 重複している場合
-    def form_invalid(self, form):
-        return redirect('market:insertResult', ans="bad")
 
 
 # 珈琲豆追加view
@@ -37,30 +19,16 @@ class InsertBeansView(generic.CreateView):
     success_url = reverse_lazy('market:top')
 
 
-# ログインページ
-class Login(LoginView):
-    form_class = LoginForm
-    template_name = 'login.html'
-
-
-# ログアウトページ
-class Logout(LoginRequiredMixin, LogoutView):
-    template_name = 'logout.html'
-
-
 class Top(generic.ListView):
     model = CoffeeBeans
     context_object_name = 'coffee_beans'
     template_name = 'top.html'
 
 
-def insert_result(request, ans):
-    context = {
-        'message' : ans
-    }
-    return render(request, 'complete.html', context)
-
-
+class BeanDetail(generic.DetailView):
+    model = CoffeeBeans
+    context_object_name = 'coffee_beans'
+    template_name = 'coffeebean_detail'
 
 
 
