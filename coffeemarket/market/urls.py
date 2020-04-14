194ef55bc45481ee64f87120b430ef335b2ae8eb
hoashi <akane.hoashi@outlook.jp>
2020-04-14 23:45:34 +0900

    豆詳細html追記

--- coffeemarket/market/urls.py
+++ coffeemarket/market/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import handler404
 from django.contrib.auth.views import LoginView, LogoutView
+from django.http import request
 from django.urls import path
 from . import views
 
@@ -8,7 +9,8 @@ app_name = 'market'
 urlpatterns = [
     # coffee関連
     path('newbeans/', views.InsertBeansView.as_view(), name='insertBeans'),
+    path('newplace/', views.InsertPlaceView.as_view(), name='insertPlace'),
     path('top/', views.Top.as_view(), name='top'),
-    # path('top/<int:pk>/', )
+    path('bean/<int:pk>/', views.BeanDetail.as_view(), name='beanDetail'),
 
 ]
