c2c19a5b45ca82c30a88d467e187b5bc71c175e0
hoashi <akane.hoashi@outlook.jp>
2020-04-15 19:53:09 +0900

    テーブル変更に伴う登録ページ変更

--- coffeemarket/market/views.py
+++ coffeemarket/market/views.py
@@ -2,14 +2,21 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.views import generic
-from .models import InsertRegister, CoffeeBeans
-from .forms import Registration, InsertBeans
+from django.http import request
+from django.urls import reverse_lazy, reverse
+from django.views import generic, View
+from django.views.decorators.http import require_POST
+from django.views.generic import TemplateView
+from .models import CoffeeBeans, PlaceCategory
+from .forms import InsertBeans, InsertPlace
 
 
 # Create your views here.
-
+#
+# def get_beans_data():
+#     result = CoffeeBeans.objects.distinct('place')
+#     result = list(result)
+#     return result
 
 # 珈琲豆追加view
 class InsertBeansView(generic.CreateView):
@@ -17,6 +24,22 @@ class InsertBeansView(generic.CreateView):
     form_class = InsertBeans
     template_name = "coffeebeans_form.html"
     success_url = reverse_lazy('market:top')
+
+
+# 地域登録 テンプレート側のnameをInsertPlaceのfield名と合わせる
+class InsertPlaceView(View):
+
+    def post(self, request):
+        context = {}
+        insert_place = InsertPlace(request.POST)
+        # 登録
+        if insert_place.is_valid():
+            insert_place.save()
+        else:
+            context = {
+                'error': 'エラー'
+            }
+        return redirect('market:insertBeans')
 
 
 class Top(generic.ListView):
@@ -27,8 +50,26 @@ class Top(generic.ListView):
 
 class BeanDetail(generic.DetailView):
     model = CoffeeBeans
-    context_object_name = 'coffee_beans'
-    template_name = 'coffeebean_detail'
+    context_object_name = 'coffee_bean'
+    template_name = 'coffeebean_detail.html'
 
-
-
+# class InsertBeansView(TemplateView):
+#
+#     insert_bean_form = InsertBeans
+#     insert_place_form = InsertPlace
+#     template_name = 'coffeebeans_form.html'
+#
+#     def post(self, request):
+#         bean_form = self.insert_bean_form(request.POST, request.FILES or None)
+#         place_form = self.insert_place_form(request.POST, request.FILES or None)
+#
+#         if bean_form.is_valid():
+#             self.bean_form.save()
+#             print('豆成功')
+#         elif place_form.is_valid():
+#             self.place_form.save()
+#             print('地域成功')
+#         else:
+#             context = {
+#                 'error': '登録に失敗しました'
+#             }
