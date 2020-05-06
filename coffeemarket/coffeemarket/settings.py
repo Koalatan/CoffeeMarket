244f808df91719a7a442ffdacb803f5f3f76de02
hoashi <akane.hoashi@outlook.jp>
2020-05-06 18:51:55 +0900

    ユーザー用画像保存先設定。

--- coffeemarket/coffeemarket/settings.py
+++ coffeemarket/coffeemarket/settings.py
@@ -129,5 +129,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = '/market/bean/list/'
 LOGIN_URL = '/accounts/login/'
