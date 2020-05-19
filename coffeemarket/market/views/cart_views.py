e58f16c0b416e5ed2802b816e7a0b9993c3d3d9f
hoashi <akane.hoashi@outlook.jp>
2020-05-19 17:31:30 +0900

    fix #7 viewsを分割

---
+++ coffeemarket/market/views/cart_views.py
@@ -0,0 +1,57 @@
+# 豆詳細　けん　カート追加処理
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import request
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+from django.views.generic import TemplateView
+
+from ..forms import InsertCart
+from ..models import CoffeeBeans, CartInfo
+
+
+class BeanDetail(TemplateView):
+    template_name = 'coffeebean_detail.html'
+
+    # initialで
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        bean_id = self.kwargs['pk']
+        coffee_beans = get_object_or_404(CoffeeBeans, id=bean_id)
+        insert_cart = InsertCart
+        context['coffee_bean'] = coffee_beans
+        context['insert_cart'] = insert_cart
+        return context
+
+    def post(self, *args, **kwargs):
+        context = self.get_context_data()
+        bean_id = self.kwargs['pk']
+        user = self.request.user
+        # coffee_beans = get_object_or_404(CoffeeBeans, id=bean_id)
+        volume = self.request.POST.get('volume')
+
+        # method unique_exists() validate
+        if CartInfo.check_unique_constrains(user, bean_id):
+            context['msg'] = 'すでにカート内に登録されています。'
+
+        else:
+            context['msg'] = 'カートへの追加完了'
+            cart_info = CartInfo(user=user, coffee_beans_id=bean_id, volume=volume)
+            cart_info.save()
+
+        return render(request, self.template_name, context)
+        # CartInfo.objects.create(user=self.request.user, coffee_beans=coffee_beans, volume=volume)
+
+
+# カート内情報閲覧
+class CartInfoList(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'cart_info.html'
+    login_url = '/accounts/login/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        user = self.request.user
+        context['cart_info_list'] = CartInfo.objects.filter(user=user)
+        context['public_key'] = settings.STRIPE_PUBLIC_KEY
+        return context
---
+++ coffeemarket/market/views/payment_views.py
@@ -0,0 +1,94 @@
+import stripe
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.utils import timezone
+from django.views import generic
+
+from ..models import PurchaseDetail, CartInfo, PurchaseHistory
+
+
+# 購入処理
+def savePurchaseDetail(purchase_history_pk, cart_info_list):
+    for cart_info in cart_info_list:
+        PurchaseDetail.objects.create(
+            purchase_code_id=purchase_history_pk, beans_code_id=cart_info.coffee_beans.pk,
+            selling_price=cart_info.coffee_beans.price, volume=cart_info.volume
+        )
+
+
+def sumTotalPrice(cart_info_list):
+    total_price = 0
+    for cart_info in cart_info_list:
+        total_price += cart_info.coffee_beans.price * cart_info.volume
+    return total_price
+
+
+class BuyingProcessView(generic.TemplateView):
+
+    def post(self, *args, **kwargs):
+        user = self.request.user
+        cart_info_list = CartInfo.objects.filter(user=user)
+        token = self.request.POST['stripeToken']
+        # total_price calculation
+        total_price = sumTotalPrice(cart_info_list)
+        try:
+            charge = stripe.Charge.create(
+                amount=total_price,
+                currency='jpy',
+                source=token,
+                description='珈琲豆売上'
+            )
+        except stripe.error.CardError as e:
+            return redirect('market:buyingError')
+        else:
+            # 購入履歴に保存
+            buy_date = timezone.now()
+            PurchaseHistory.objects.create(
+                user_name=user, total_price=total_price, payment_code_id=1, buy_date=buy_date
+            )
+            purchase_history_pk = \
+                PurchaseHistory.objects.filter(user_name=user, buy_date=buy_date).values_list('id', flat=True)
+
+            if len(purchase_history_pk) != 1:
+                print(purchase_history_pk)
+                print("err!")
+            else:
+                savePurchaseDetail(purchase_history_pk[0], cart_info_list)
+            return redirect('market:buyingSuccess')
+
+
+class PurchaseView(LoginRequiredMixin, generic.ListView):
+    context_object_name = 'purchase_historys'
+    template_name = 'purchase_history.html'
+
+    def get_queryset(self):
+        user = self.request.user
+        query_set = PurchaseHistory.objects.filter(user_name=user)
+        return query_set
+
+
+class PurchaseHistoryDetailView(LoginRequiredMixin, generic.ListView):
+    context_object_name = 'purchase_details'
+    template_name = 'purchase_detail.html'
+
+    def get_queryset(self):
+        user = self.request.user
+        purchase_id = self.kwargs['pk']
+        query_set = PurchaseDetail.objects.filter(
+            purchase_code_id=purchase_id, purchase_code__user_name=user
+        )
+        # select_related('beans_code', 'purchase_code', 'purchase_code__user_name').\
+        # get(purchase_code=purchase_id, id=user.pk)
+
+        return query_set
+
+
+
+# 決済エラー
+class BuyingErrorView(generic.TemplateView):
+    template_name = 'buying_error.html'
+
+
+# 決済完了
+class BuyingSuccessView(generic.TemplateView):
+    template_name = 'buying_success.html'
