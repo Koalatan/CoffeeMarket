21724a90407b6bcae48bdcac5bc52a1115dc20b8
hoashi <akane.hoashi@outlook.jp>
2020-06-21 22:41:26 +0900

    追加：会員のプロファイルモデル

--- coffeemarket/accounts/models.py
+++ coffeemarket/accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, blank=True)
+    zip_code = models.CharField(max_length=8, blank=True)
+    tel = models.CharField(max_length=12, blank=True)
