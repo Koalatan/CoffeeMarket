ec7e993ada55ed61d8362f42913edbd30bff8fdf
hoashi <akane.hoashi@outlook.jp>
2020-06-21 22:56:28 +0900

    変更：User作成時、更新時Profileも変更されるようシグナルを設定

--- coffeemarket/accounts/models.py
+++ coffeemarket/accounts/models.py
@@ -1,10 +1,24 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=100, blank=True)
     zip_code = models.CharField(max_length=8, blank=True)
     tel = models.CharField(max_length=12, blank=True)
+
+
+# シグナル@receiver 今回はUserに対してcreateが入力された時に実行される。
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+# シグナル@receiver 今回はUserに対してsaveが入力された時に実行される。
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
