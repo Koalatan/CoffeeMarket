caa2762b44a196953adb3bf9051a8fe1b672eb61
hoashi <akane.hoashi@outlook.jp>
2020-06-28 22:10:34 +0900

    追加：ユーザー情報閲覧用view
    初めてのcached_property

--- coffeemarket/accounts/views.py
+++ coffeemarket/accounts/views.py
@@ -1,7 +1,13 @@
+from django import views
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.utils.functional import cached_property
+from django.views import generic
+
+from . import models
 from .forms import LoginForm
+
 
 # ログインページ
 
@@ -14,3 +20,15 @@ class Login(LoginView):
 # ログアウトページ
 class Logout(LoginRequiredMixin, LogoutView):
     template_name = 'logout.html'
+
+
+# 会員情報閲覧用ページ
+class UserInfo(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'user_info_detail.html'
+
+    # cached_propertyをつけることにより
+    # 2回目以降template側から呼び出した際に、
+    # sqlが発行されずキャッシュから返すようになる
+    @cached_property
+    def userInfo(self):
+        return models.Profile.objects.filter(user=self.request.user).first()
