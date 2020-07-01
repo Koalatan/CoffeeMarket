8f72d93aa0fff129dab0b47d8f4203d9400461d7
hoashi <akane.hoashi@outlook.jp>
2020-07-02 00:57:17 +0900

    追加：プロファイル変更用Form

--- coffeemarket/accounts/forms.py
+++ coffeemarket/accounts/forms.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from .models import Profile
 
 
 # ログインフォーム
@@ -8,3 +10,20 @@ class LoginForm(AuthenticationForm):
 
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('address', 'zip_code', 'tel')
+        labels = {
+            'address': '住所',
+            'zip_code': '郵便番号',
+            'tel': '電話番号',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['address'].widget.attrs['class'] = 'form-control'
+        self.fields['zip_code'].widget.attrs['class'] = 'form-control'
+        self.fields['tel'].widget.attrs['class'] = 'form-control'
